5fa364ad0f7099dda63cdaaed5f890c9bcfb8f1e
hansel zhao <zhaoshouhang9295@icloud.com>
2024-08-21 17:02:57 +0800

    Algebraic Data Types homework

--- 02ADTs/LogAnalysis.hs
+++ 02ADTs/LogAnalysis.hs
@@ -2,8 +2,9 @@
 
 module LogAnalysis where
 
-import Log (LogMessage (..), MessageType (..), testParse)
+import Log (LogMessage (..), MessageTree (..), MessageType (..), testParse, testWhatWentWrong)
 
+-- exercise 1
 -- 提取某个位置上的单词 pos 从1开始
 takeWords :: String -> Int -> String
 takeWords s pos
@@ -24,3 +25,55 @@ parseMessage s =
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
+
+-- exercise2
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) mt = mt
+insert lm Leaf = Node Leaf lm Leaf
+insert _ (Node _ (Unknown _) _) = Leaf
+insert lm@(LogMessage _ t _) (Node mt1 logMsg@(LogMessage _ t1 _) mt2)
+  -- 必须生成新的 才行 之前直接使用insert lm mt1 就是错误的
+  | t1 > t = Node (insert lm mt1) logMsg mt2
+  | otherwise = Node mt1 logMsg (insert lm mt2)
+
+-- exercise3
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build [x] = insert x Leaf
+build (x : xs) = insert x (build xs)
+
+-- exercise4
+
+inOrder :: MessageTree -> [LogMessage]
+-- 处理空
+inOrder Leaf = []
+-- 这段代码的问题是用了多余的模式匹配,另外 最后处理 node node 情况的时候没有处理lm的数据导致漏掉了一些数据
+-- inOrder
+--   (Node mtl lm mtr) =
+--     case (mtl, mtr) of
+--       (Leaf, Leaf) -> [lm]
+--       (Node {}, Leaf) -> inOrder mtl ++ [lm]
+--       (Leaf, Node {}) -> lm : inOrder mtr
+--       (Node {}, Node {}) -> inOrder mtl ++ inOrder mtr
+-- 修改后
+inOrder
+  (Node mtl lm mtr) = inOrder mtl ++ [lm] ++ inOrder mtr
+
+-- exercise5
+errMoreThan50 :: LogMessage -> Bool
+errMoreThan50 l = case l of
+  Unknown _ -> False
+  LogMessage m _ _ -> case m of
+    Error i -> i > 50
+    _ -> False
+
+getMsg :: LogMessage -> String
+getMsg l = case l of
+  Unknown _ -> ""
+  LogMessage _ _ s -> s
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong [Unknown _] = []
+whatWentWrong s = map getMsg (filter errMoreThan50 (inOrder (build s)))
