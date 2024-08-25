24cba186d99dd66044974bd783f334f30e95b0e8
hansel zhao <zhaoshouhang9295@icloud.com>
2024-08-25 20:30:56 +0800

    03

--- 03-rec-poly/recpoly.hs
+++ 03-rec-poly/recpoly.hs
@@ -27,3 +27,30 @@ result1 = mapIntList abs exampleList
 result2 = mapIntList square exampleList
 
 -- 接下来是filter
+keepOnlyEven :: IntList -> IntList
+keepOnlyEven Empty = Empty
+keepOnlyEven (Cons x xs)
+  | even x = Cons x (keepOnlyEven xs)
+  | otherwise = keepOnlyEven xs
+
+-- 这里就是多态的使用,可以理解成泛型 t代表类型
+data List t = E | C t (List t)
+
+lst1 :: List Int
+lst1 = C 3 (C 5 (C 2 E))
+
+lst2 :: List Char
+lst2 = C 'x' (C 'y' (C 'z' E))
+
+lst3 :: List Bool
+lst3 = C True (C False E)
+
+filterList _ E = E
+filterList p (C x xs)
+  | p x = C x (filterList p xs)
+  | otherwise = filterList p xs
+
+-- map的定义 既可以从a->b 变成不同类型,也可以把b变成a 认为是相同类型的转换
+mapList :: (a -> b) -> List a -> List b
+mapList _ E = E
+mapList f (C x xs) = C (f x) (mapList f xs)
